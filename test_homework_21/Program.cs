1744625e25fed79ad585994c6f78dde4062325d4
Leonid Drobotov <fromarshincevo95@gmail.com>
2023-07-09 20:33:23 +0300

    Added a solutoin for homework task 21

---
+++ test_homework_21/Program.cs
@@ -0,0 +1,21 @@
+﻿double Distance_3D(int XA, int YA, int ZA, int XB, int YB, int ZB)
+{
+double distance = Math.Sqrt(Math.Pow(XB - XA,2)+Math.Pow(YB - YA,2)+Math.Pow(ZB - ZA,2));
+return distance;
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int coordX_A = GetInfo("Введите координату X точки A : ");
+int coordY_A = GetInfo("Введите координату Y точки A : ");
+int coordZ_A = GetInfo("Введите координату Z точки A : ");
+int coordX_B = GetInfo("Введите координату X точки B : ");
+int coordY_B = GetInfo("Введите координату Y точки B : ");
+int coordZ_B = GetInfo("Введите координату Z точки B : ");
+
+Console.WriteLine($"Расстояние между точками в пространстве составляет: {Distance_3D(coordX_A, coordY_A, coordZ_A, coordX_B, coordY_B, coordZ_B)}");
+
