efa8b11178219ac44f08a8a5d3da208f717f5353
Leonid Drobotov <fromarshincevo95@gmail.com>
2023-07-09 22:11:27 +0300

    We are found a solution for task with Palindrom

---
+++ test_homework_19/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палидромом
+
+string Palindrome (string input_user_num)
+{
+    if (input_user_num [0] == input_user_num [4] & input_user_num [1] == input_user_num [3]) return "Да";
+    else return "нет";
+}
+
+string GetInfo(string message)
+{
+    Console.Write(message);
+    return Console.ReadLine();
+}
+
+string user_num = GetInfo("Введите пятизначное число: ");
+if (user_num.Length != 5) Console.WriteLine("Введено неверное число");
+else Console.WriteLine($"{user_num} -> {Palindrome(user_num)}");
