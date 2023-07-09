cb606f87e4df3893364f317a6a00c6eb4c8c95da
Leonid Drobotov <fromarshincevo95@gmail.com>
2023-07-09 21:05:21 +0300

    Added a solition for task 23

---
+++ test_homework_23/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдает таблицу кубов чисел от 1 до N
+
+void Cube(int N)
+{
+Console.Write($"{N} -> ");
+for (int i = 1;i < N; i++)
+{
+    Console.Write(Math.Pow(i, 3) + ", ");
+}
+Console.WriteLine(Math.Pow(N,3) + ". ");
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int N = GetInfo("Введите значение: ");
+Cube (N);
